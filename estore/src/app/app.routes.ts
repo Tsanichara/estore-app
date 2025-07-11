d9a7b1aa0145c3f4846fec3ee2edcacdaf54d5f3
Tyler Sanichara <tsanichara67@gmail.com>
2025-07-11 19:32:44 -0400

    Created 404 error page

--- estore/src/app/app.routes.ts
+++ estore/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home'
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
 
     {path: 'home', component: Home},
-    {path: '', redirectTo: '/home', pathMatch: 'full'}
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: NotFound},
 
 ];
