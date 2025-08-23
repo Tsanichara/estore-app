7641645d865bba076b3b4984a1cfb8f7009b6741
Tyler Sanichara <tsanichara67@gmail.com>
2025-08-23 15:59:41 -0400

    set up dynamic rendering of product details

--- estore/src/app/components/home/product-details/product-details.ts
+++ estore/src/app/components/home/product-details/product-details.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Ratings } from "../../ratings/ratings";
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../services/product/products-service';
+import { Product } from '../types/productsType';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 
 @Component({
   selector: 'app-product-details',
@@ -8,5 +13,21 @@ import { Ratings } from "../../ratings/ratings";
   styleUrl: './product-details.css'
 })
 export class ProductDetails {
+  private readonly route = inject(ActivatedRoute);
+  private readonly productsService = inject(ProductsService);
 
+  readonly product = signal<Product | null>(null);
+
+  constructor() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    const id = idParam ? Number(idParam) : null;
+
+    if(id !== null && !isNaN(id)) {
+      this.productsService.getProduct(id).pipe(takeUntilDestroyed()).subscribe((res) => {
+        this.product.set(Array.isArray(res) ? res[0] : res);
+      });
+      return;
+    }
+
+  }
 }
