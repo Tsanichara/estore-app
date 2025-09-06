491043384b82192a61f902569948c45695415538
Tyler Sanichara <tsanichara67@gmail.com>
2025-09-06 16:27:06 -0400

    Create user sign up API

--- estore-server/index.js
+++ estore-server/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const cors = require('cors');
 const productCategories = require("./routes/productCategories");
 const products = require("./routes/products")
+const user = require('./routes/users');
+const bodyParser = require('body-parser');
 require("dotenv").config();
 const app = express();
 const PORT = 5001;
 
 app.use(cors());
+app.use(bodyParser.json());
+
 app.use("/productCategories", productCategories);
 app.use("/products", products);
+app.use("/users", user);
 
 const server = app.listen(PORT, () => {
   console.log("App is running on the port - 5001");
