8512eba79a7552008c0db899f07021cc81617b31
Tyler Sanichara <tsanichara67@gmail.com>
2025-09-23 20:16:07 -0400

    Display delivery address in checkout screen

--- estore/src/app/components/home/cart/cart.ts
+++ estore/src/app/components/home/cart/cart.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal, WritableSignal } from '@angular/core';
 import { faTrash, faBoxOpen, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { CartItem } from '../types/cart.type';
 import { Router } from '@angular/router';
 import { CartStoreItem } from '../services/cart/cart.storeitem';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Ratings } from "../../ratings/ratings";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoggedInUser } from '../types/user.type';
+import { UserService } from '../services/user/userService';
 
 
 @Component({
   selector: 'app-cart',
-  imports: [FontAwesomeModule, Ratings],
+  imports: [FontAwesomeModule, Ratings, ReactiveFormsModule],
   templateUrl: './cart.html',
   styleUrl: './cart.css'
 })
@@ -18,8 +21,38 @@ export class Cart {
   faBoxOpen = faBoxOpen;
   faShoppingCart = faShoppingCart;
 
-  constructor(public cartStore: CartStoreItem, private router: Router) {
+  user = signal<LoggedInUser>({
+    firstName: '',
+    lastName: '',
+    address: '',
+    city: '',
+    state: '',
+    pin: '',
+  })
 
+  orderForm: WritableSignal<FormGroup>;
+
+  constructor(public cartStore: CartStoreItem, private router: Router, private userService: UserService, private fb: FormBuilder) {
+    this.orderForm = signal(this.createOrderForm(this.user()));
+    this.userService.loggedInUser$.subscribe((u) => this.user.set(u));
+
+    effect(() => {
+      const newUser = this.user();
+      this.orderForm.set(this.createOrderForm(newUser));
+    })
+  }
+
+  private createOrderForm(user: LoggedInUser | null): FormGroup {
+    return this.fb.group({
+      name: [
+        user?.firstName && user?.lastName ? `${user.firstName} ${user.lastName}`.trim() : '',
+        Validators.required,
+      ],
+      address: [user?.address || '', Validators.required],
+      city: [user?.city || '', Validators.required],
+      state: [user?.state || '', Validators.required],
+      pin: [user?.pin || '', Validators.required],
+    })
   }
 
   navigateToHome(): void {
@@ -39,4 +72,11 @@ export class Cart {
     this.cartStore.removeProduct(cartItem);
   }
 
+  onSubmit(): void {
+    if (this.orderForm().valid){
+      console.log('Order submitted: ', this.orderForm().value);
+    } else {
+      console.log('Invalid order form');
+    }
+  }
 }
