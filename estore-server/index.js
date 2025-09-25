66263ad39492091ededad55420985e3a8727cc5c
Tyler Sanichara <tsanichara67@gmail.com>
2025-09-24 20:12:07 -0400

    Create API to save authenticated orders

--- estore-server/index.js
+++ estore-server/index.js
@@ -4,6 +4,7 @@ const productCategories = require("./routes/productCategories");
 const products = require("./routes/products")
 const user = require('./routes/users');
 const bodyParser = require('body-parser');
+const orders = require("./routes/orders");
 require("dotenv").config();
 const app = express();
 const PORT = 5001;
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use("/productCategories", productCategories);
 app.use("/products", products);
 app.use("/users", user);
+app.use("/orders", orders);
 
 const server = app.listen(PORT, () => {
   console.log("App is running on the port - 5001");
---
+++ estore-server/shared/checkToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const checkToken = async(req, res, next) => {
+    try {
+        const token =  req.headers?.authorization;
+
+        if (!token) {
+            return res.status(403).json({message: "No token provided."});
+        }
+
+        await jwt.verify(token, "estore-secret-key");
+
+        next();
+
+    } catch {
+        console.log("JWT verification failed: ", error.message);
+        res.status(401).json({message: "Authorization failed!"})
+    }
+};
+
+module.exports = checkToken;
