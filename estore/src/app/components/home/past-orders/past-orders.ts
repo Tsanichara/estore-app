c0e94e2e6f31ba79dedb3c72884a96fec62d90c6
Tyler Sanichara <tsanichara67@gmail.com>
2025-09-26 19:53:37 -0400

    Implement functionality to display past orders

--- estore/src/app/components/home/past-orders/past-orders.ts
+++ estore/src/app/components/home/past-orders/past-orders.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PastOrder, PastOrderProduct } from '../types/order.type';
 
 @Component({
   selector: 'app-past-orders',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './past-orders.css'
 })
 export class PastOrders {
+  pastOrderProducts: PastOrderProduct[] = [
+    {
+    amount: 100,
+    price: 50,
+    productId: 1,
+    productImage: 'shop-1.jpg',
+    productName: 'Jacket',
+    qty: 1,
+    }
+  ];
+
+  pastOrder: PastOrder = {
+    address: 'Sample Address',
+    city: 'JC',
+    orderDate: '03/01/23',
+    pin: '12345',
+    state: 'NY',
+    total: 100,
+    userName: 'Thomas Brown'
+  };
+
 
 }
